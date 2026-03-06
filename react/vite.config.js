9e46be77632dc3d7ebc784d985fb8e4c24fda03b
kento-ix <kentoix1105@gmail.com>
2026-03-06 14:25:04 -0800

    feat: display User info to frontend(dummy data)

--- react/vite.config.js
+++ react/vite.config.js
@@ -1,14 +1,25 @@
 // react/vite.config.js
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    watch: {
-      usePolling: true
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [react()],
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+      watch: {
+        usePolling: true
+      },
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
     }
   }
 })
