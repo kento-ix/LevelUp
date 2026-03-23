926bfb09a292e572904f3e7bfa9250d3ad4bedf4
kento-ix <kentoix1105@gmail.com>
2026-03-23 14:11:09 -0700

    feat: add simple style and setup routing on App.js

---
+++ react/src/pages/FriendsPage.jsx
@@ -0,0 +1,11 @@
+import FriendList from "../components/FriendList";
+import AddFriendForm from "../components/form/AddFriendForm";
+
+export default function FriendsPage() {
+  return (
+    <>
+      <AddFriendForm />
+      <FriendList />
+    </>
+  );
+}
