d932c2b8a2daf514225371f01a55b51a0823c81c
kento-ix <kentoix1105@gmail.com>
2026-03-27 11:59:05 -0700

    feat:set post all event as landing page

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Layout from './components/layout/Layout'
+import PostsPage from './pages/PostsPage'
+import PostsByCommunityPage from './pages/PostsByCommunityPage'
 import UsersPage from './pages/UsersPage'
 import FriendsPage from './pages/FriendsPage'
 
@@ -9,6 +11,8 @@ function App() {
     <BrowserRouter>
       <Layout>
         <Routes>
+          <Route path="/" element={<PostsPage />} />
+          <Route path="/posts/community/:id" element={<PostsByCommunityPage />} />
           <Route path="/users" element={<UsersPage />} />
           <Route path="/friends" element={<FriendsPage />} />
         </Routes>
