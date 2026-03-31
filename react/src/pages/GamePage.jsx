a30c9f81b793f28736b7f884142c1925793f4f17
Danielle Magnanao <dnlmagnanao@gmail.com>
2026-03-31 10:48:50 -0700

    created folder for games and created gamePage.jsx

---
+++ react/src/pages/GamePage.jsx
@@ -0,0 +1,12 @@
+import GameList from "../components/games/GameList";
+import PageHeader from "../components/layout/PageHeader";
+
+export default function GamePage() {
+  return (
+    <>
+      <PageHeader title="Games" />
+      {/* <AddFriendForm /> */}
+      <GameList />
+    </>
+  );
+}
