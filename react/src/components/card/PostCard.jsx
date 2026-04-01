64d18ecbc5dbb7434df65dd1e0ddf1bc4361f195
Danielle Magnanao <dnlmagnanao@gmail.com>
2026-03-31 17:26:19 -0700

    user can now get all posts in a community using communityID

--- react/src/components/card/PostCard.jsx
+++ react/src/components/card/PostCard.jsx
@@ -6,12 +6,12 @@ export default function PostCard(props) {
 
   return (
     <li onClick={() => navigate(`/posts/community/${post.CommunityID}`)} style={{ cursor: "pointer", border: "1px solid #ccc", borderRadius: "6px", padding: "0.75rem", listStyle: "none" }}>
+      <div><strong>PostID:</strong> {post.PostID}</div>
       <div><strong>Title:</strong> {post.Title}</div>
       <div><strong>Username:</strong> {post.Username}</div>
-      <div><strong>Community:</strong> {post.CommunityName}</div>
-      <div><strong>Genre:</strong> {post.Genre}</div>
       <div><strong>Date:</strong> {post.Date_Created}</div>
       <div><strong>Content:</strong> {post.Content}</div>
+      <div><strong>CommunityID:</strong> {post.CommunityID}</div>
     </li>
   );
 }
