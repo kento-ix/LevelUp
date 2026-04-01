7386fde0df0408757dd4aa1b4274d178b9c5e692
Danielle Magnanao <dnlmagnanao@gmail.com>
2026-03-31 17:38:21 -0700

    Added CommunityCard to show all communities in the database and CommunityList. Edited CommunitiesPage to include CommunityList.

---
+++ react/src/components/card/CommunityCard.jsx
@@ -0,0 +1,10 @@
+export default function CommunityCard({ community }) {
+  return (
+    <li style={{ border: "1px solid #ccc", borderRadius: "6px", padding: "0.75rem", listStyle: "none" }}>
+      <div><strong>CommunityID:</strong> {community.CommunityID}</div>
+      <div><strong>Name:</strong> {community.Name}</div>
+      <div><strong>Description:</strong> {community.Description}</div>
+      <div><strong>Game:</strong> {community.GameTitle}</div>
+    </li>
+  );
+}
